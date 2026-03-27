ffde51f3f8c02871fc9a0a4fbd453806f28d54ed
diprojose <parejojose77@gmail.com>
2026-03-27 16:19:57 -0500

    feat: added lazy loading to prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,8 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
-    await this.$connect();
+    // Eliminamos await this.$connect() para que Prisma use "lazy connections"
+    // lo cual recupera mejor las conexiones cuando un proxy (como Supabase) las corta por inactividad.
   }
 
   async onModuleDestroy() {
